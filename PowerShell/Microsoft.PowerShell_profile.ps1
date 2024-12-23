425ef1c0e6c65a82c1d74a273a08ba6acc34c3fa
Joseph Hanson <joe@veri.dev>
2024-12-22 18:09:57 -0600

    swap terminal icons for lsd, swap oh-my-posh with starship. Add CommandNotFound.

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,7 +2,16 @@
 # What the heck windows? Why is where an alias for Where-Object? Be normal!
 Remove-Item -Path Alias:where -Force
 Set-Alias -Name where -Value where.exe -Force
+# ls --> lsd
+Set-Alias ls lsd
+function l { lsd -l $args }
+function la { lsd -a $args }
+function lla { lsd -la $args }
+function lt { lsd --tree $args }
 # https://github.com/ChrisTitusTech/winutil
 Set-Alias -Name winutil -Value Invoke-WinUtil
-# https://github.com/devblackops/Terminal-Icons
-Import-Module -Name Terminal-Icons
+# Starship init
+Invoke-Expression (&starship init powershell)
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
