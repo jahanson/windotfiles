1b80fa16c700546f5539d302ff7603eb5134a257
Joseph Hanson <joe@veri.dev>
2024-12-28 17:39:15 -0600

    comment blocks

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,43 +1,40 @@
 . $PSScriptRoot/Scripts/hsndev/functions.ps1
 
-## Aliases
-# Linux-like aliases
-# which/where
-
+<# Aliases
+ Linux-like aliases which/where
+#>
 Remove-Item -Path Alias:where -Force
 Set-Alias -Name where -Value where.exe -Force
 Set-Alias which where.exe
-# grep
 
+<# grep #>
 Set-Alias grep Select-String
-# ls --> lsd
 
+<# ls --> lsd #>
 Set-Alias ls lsd
 function l { lsd -l $args }
 function la { lsd -a $args }
 function lla { lsd -la $args }
 function lt { lsd --tree $args }
-# https://github.com/ChrisTitusTech/winutil
 
+<# https://github.com/ChrisTitusTech/winutil #>
 Set-Alias -Name winutil -Value Invoke-WinUtil
 
+<# printenv  #>
 function Show-Environment {
   Get-ChildItem Env: | Format-Table -AutoSize
 }
-
-# Create the alias
-
 Set-Alias -Name printenv -Value Show-Environment
 
-# Mise activate
+<# Doggo - https://github.com/mr-karan/doggo #>
+Set-Alias -Name dig -Value doggo.exe -Force
 
+<# Mise activate #>
 (&mise activate pwsh) | Out-String | Invoke-Expression
-# Starship init
 
+<# Starship init #>
 Invoke-Expression (&starship init powershell)
 
 #f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
-
 Import-Module -Name Microsoft.WinGet.CommandNotFound
-
 #f45873b3-b655-43a6-b217-97c00aa0db58
