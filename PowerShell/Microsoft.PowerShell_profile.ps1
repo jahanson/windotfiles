160d8b3e155c81f0be757193dde4081aaf907910
Joseph Hanson <joe@veri.dev>
2024-12-28 13:08:56 -0600

    add more commands, correct alias, and activate mise

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,21 +1,43 @@
 . $PSScriptRoot/Scripts/hsndev/functions.ps1
+
+## Aliases
 # Linux-like aliases
 # which/where
+
 Remove-Item -Path Alias:where -Force
 Set-Alias -Name where -Value where.exe -Force
-New-Alias which where.exe
+Set-Alias which where.exe
 # grep
-New-Alias grep Select-String
+
+Set-Alias grep Select-String
 # ls --> lsd
+
 Set-Alias ls lsd
 function l { lsd -l $args }
 function la { lsd -a $args }
 function lla { lsd -la $args }
 function lt { lsd --tree $args }
 # https://github.com/ChrisTitusTech/winutil
+
 Set-Alias -Name winutil -Value Invoke-WinUtil
+
+function Show-Environment {
+  Get-ChildItem Env: | Format-Table -AutoSize
+}
+
+# Create the alias
+
+Set-Alias -Name printenv -Value Show-Environment
+
+# Mise activate
+
+(&mise activate pwsh) | Out-String | Invoke-Expression
 # Starship init
+
 Invoke-Expression (&starship init powershell)
+
 #f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
 Import-Module -Name Microsoft.WinGet.CommandNotFound
+
 #f45873b3-b655-43a6-b217-97c00aa0db58
