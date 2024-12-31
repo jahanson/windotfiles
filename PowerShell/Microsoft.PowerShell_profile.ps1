891e2e228df4f3f9b760b077d9662295c8a82019
Joseph Hanson <joe@veri.dev>
2024-12-31 11:12:35 -0600

    sub background op for var sub

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -30,10 +30,12 @@ Set-Alias -Name printenv -Value Show-Environment
 Set-Alias -Name dig -Value doggo.exe -Force
 
 <# Mise activate #>
-(&mise activate pwsh) | Out-String | Invoke-Expression
+$(mise activate pwsh) | Out-String | Invoke-Expression
 
 <# Starship init #>
 Invoke-Expression (&starship init powershell)
+<# github-cli completion #>
+$(gh completion -s powershell) | Out-String | Invoke-Expression
 
 #f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
 Import-Module -Name Microsoft.WinGet.CommandNotFound
